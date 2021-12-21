c6be1f81303d4e770dea4c8f62fb6454e647a076
Wahyu Hauzan Rafi <57913209+whauzan@users.noreply.github.com>
2021-12-22 00:48:45 +0700

    feat: request class company & employee

--- src/main/java/com/pbo/pmo/Pojos/CompanyRequest.java
+++ src/main/java/com/pbo/pmo/Pojos/CompanyRequest.java
@@ -1,2 +1,9 @@
-package com.pbo.pmo.Pojos;public class CompanyRequest {
+package com.pbo.pmo.Pojos;
+
+public class CompanyRequest {
+    public int id;
+    public String name;
+    public String phoneNumber;
+    public String email;
+    public String address;
 }
--- src/main/java/com/pbo/pmo/Pojos/EmployeeRequest.java
+++ src/main/java/com/pbo/pmo/Pojos/EmployeeRequest.java
@@ -1,2 +1,11 @@
-package com.pbo.pmo.Pojos;public class EmployeeRequest {
+package com.pbo.pmo.Pojos;
+
+public class EmployeeRequest {
+    public int id;
+    public String name;
+    public char gender;
+    public String email;
+    public String address;
+    public String phoneNumber;
+    public int company_id;
 }
