eab400a9f04d4c79cfce44e1834c4f0e22f66eba
Wahyu Hauzan Rafi <57913209+whauzan@users.noreply.github.com>
2021-12-22 00:49:38 +0700

    feat: repository company

--- src/main/java/com/pbo/pmo/repository/CompanyRepository.java
+++ src/main/java/com/pbo/pmo/repository/CompanyRepository.java
@@ -1,2 +1,9 @@
-package com.pbo.pmo.repository;public interface CompanyRepository {
+package com.pbo.pmo.repository;
+
+import com.pbo.pmo.model.Company;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CompanyRepository extends JpaRepository<Company,Integer> {
 }
--- src/main/java/com/pbo/pmo/repository/EmployeeRepository.java
+++ src/main/java/com/pbo/pmo/repository/EmployeeRepository.java
@@ -4,6 +4,9 @@ import com.pbo.pmo.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
+    List<Employee> findAllById(int company_id);
 }
